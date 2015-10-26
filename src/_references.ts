a3b12add7148229c510368e799115dee2efcadc3
edsilv <e.silverton@gmail.com>
2015-10-26 14:05:39 +0000

    updated refs

--- src/_references.ts
+++ src/_references.ts
@@ -1,3 +1,4 @@
+/// <reference path="./Engine/ClockTimer.ts" />
 /// <reference path="./Primitives/Vector.ts" />
 /// <reference path="./Exceptions/Exceptions.ts" />
 /// <reference path="./Collections/ObservableCollection.ts" />
